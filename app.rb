b9d91ee733d13153a5be97bd353e9e08bd3c88a7
Brandon Brasson <rocksong622@gmail.com>
2019-11-13 07:11:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,10 +14,10 @@ module FormsLab
      post '/pirates' do
        @pirate = Pirate.new(params[:pirate])
 
-       params[:pirate][:ships].each do |details|
+       params[:pirate][:name].each do |details|
          Ship.new(details)
        end
-       @ships = Ship.all
+       @name = Name.all
 
        erb :'pirates/show'
      end
